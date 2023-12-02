664c4a2362240936bb9cd40cb81c21b2c2bc2c73
ArtsyomPinevich <artem2000z.pinevitch@yandex.by>
2023-12-02 20:05:45 +0300

    HeaderTemplate

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import './NavBar.scss';
+import { IoCartOutline, IoSearchOutline } from 'react-icons/io5';
 
-const NavBar = () => {
+const Header = () => {
     return (
         <header className='header'>
-            <div className='header__searchbar'></div>
+            <div className='header__searchbar'>
+                <input type='text' placeholder='Search item' />
+                <button>
+                    <IoSearchOutline />
+                </button>
+            </div>
+
+            <div className='Header__userCart'>
+                <button>
+                    <IoCartOutline />
+                    user cart
+                </button>
+            </div>
         </header>
     );
 };
 
-export default NavBar;
+export default Header;
