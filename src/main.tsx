ea14dde12c3cef721d97c6b3c2c35aeb236a4766
Artsyom <artem2000z.pinevitch@yandex.by>
2024-01-19 02:57:22 +0300

    lowered ammount of toasts on sreen

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import App from './App.tsx';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.scss';
 
 import React from 'react';
@@ -7,13 +8,11 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ToastContainer } from 'react-toastify';
 
-import 'react-toastify/dist/ReactToastify.css';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ChakraProvider>
             <App />
         </ChakraProvider>
-        <ToastContainer limit={3} />
+        <ToastContainer limit={5} />
     </React.StrictMode>
 );
